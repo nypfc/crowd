3d5474815cadcbebb925b95a474d7994dd70ffe7
snownersd <snownersd@126.com>
2018-10-15 14:45:06 +0800

    2018-10-15 14:44:59

--- src/main/java/com/gedoumi/crowd/user/controller/ApiLoginController.java
+++ src/main/java/com/gedoumi/crowd/user/controller/ApiLoginController.java
@@ -1,13 +1,14 @@
 package com.gedoumi.crowd.user.controller;
 
-import com.gedoumi.crowd.user.dataobj.form.LoginForm;
+import com.gedoumi.crowd.common.utils.ContextUtil;
 import com.gedoumi.crowd.common.utils.ResponseObject;
+import com.gedoumi.crowd.user.dataobj.form.LoginForm;
+import com.gedoumi.crowd.user.dataobj.model.User;
+import com.gedoumi.crowd.user.dataobj.vo.UserVO;
 import com.gedoumi.crowd.user.service.UserService;
 import com.google.common.collect.Maps;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.BeanUtils;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.validation.Valid;
@@ -36,6 +37,19 @@ public class ApiLoginController {
         HashMap<Object, Object> map = Maps.newHashMap();
         map.put("token", userService.userLogin(loginForm));
         return ResponseObject.setSuccessResponse(map);
+    }
+
+    /**
+     * 获取用户数据
+     *
+     * @return ResponseObject
+     */
+    @GetMapping("/get")
+    public ResponseObject getUser() {
+        User user = ContextUtil.getUserFromRequest();
+        UserVO userVO = new UserVO();
+        BeanUtils.copyProperties(user, userVO);
+        return ResponseObject.setSuccessResponse(userVO);
     }
 
 }
---
+++ src/main/java/com/gedoumi/crowd/user/dataobj/vo/UserVO.java
@@ -0,0 +1,23 @@
+package com.gedoumi.crowd.user.dataobj.vo;
+
+import lombok.Data;
+
+/**
+ * 用户信息VO
+ *
+ * @author Minced
+ */
+@Data
+public class UserVO {
+
+    /**
+     * 昵称
+     */
+    private String nickname;
+
+    /**
+     * 头像
+     */
+    private String avatar;
+
+}
